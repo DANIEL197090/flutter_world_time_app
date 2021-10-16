e2233cf406892f8eac28ff9164671bc162c79509
DANIEL197090 <ifeanyimbata3@gmail.com>
2021-10-16 21:39:06 +0100

    Implement flutter spinkit for loading screen and display time in home screen

--- world_time_app/lib/pages/home.dart
+++ world_time_app/lib/pages/home.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/services/world_time.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
+    final data = ModalRoute.of(context)!.settings.arguments as Map;
+    print(data);
+
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            FlatButton.icon(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location')
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(Icons.edit_location),
+                  label: Text('Edit Location')
+              ),
+              SizedBox(height: 20.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                   Text(
+                     data['location'],
+                     style: TextStyle(
+                       fontSize: 28.0,
+                       letterSpacing: 2.0
+                     ),
+                   ),
+                ],
+              ),
+              SizedBox(height: 20.0,),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  fontSize: 65.0
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- world_time_app/lib/pages/services/world_time.dart
+++ world_time_app/lib/pages/services/world_time.dart
@@ -1,12 +1,15 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 class WorldTime {
   String location; // location name for the UI
   String time = ''; // time in that location
   String flag; // url to an asset flag icon
   String url; // location url
+  bool isDaytime; // true or false if daytime or not
 
-  WorldTime({required this.location, required this.flag, required this.url});
+  WorldTime({required this.location, required this.flag, required this.url, required this.isDaytime
+   });
 
   Future<void> getTime() async {
     try {
@@ -18,7 +21,8 @@ class WorldTime {
       DateTime now = DateTime.parse(dateTime);
       now = now.add(Duration(hours: int.parse(offSet)));
       // set the time property
-      time = now.toString();
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
     }catch(e){
       print('error $e');
       time = 'Could not get time data';
